af15c346d5a3d73559e39ad3c4c616b3ea7bd663
Michał Krause <michalkr53@gmail.com>
2024-09-28 18:49:23 +0200

    Main view, react-routing (#7)

--- frontend/wellness-garden/src/main.tsx
+++ frontend/wellness-garden/src/main.tsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
 import './static/css/index.css'
+import MainView from './components/views/MainView/MainView.tsx'
+import CssBaseline from '@mui/material/CssBaseline'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainView />
+  },
+  {
+    path: "/garden",
+    element: <div>garden</div>    // Replace with garden view
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CssBaseline />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
