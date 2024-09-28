23181a10cf63c9f9888a3890b99a9e0e5c60c84f
Konrad Bryłowski <59873325+kobryl@users.noreply.github.com>
2024-09-28 20:14:52 +0200

    API endpoints (#11)
    * /journal endpoint
    
    * rest of the endpoints
    
    * enpoints
    
    ---------
    
    Co-authored-by: Lukasz Nowakowski <lukrzak@wp.pl>

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,103 @@
-import os
+from flask import Flask, request
 
-from flask import Flask
+from backend.db.models import Journal, User, db, Activity, Friend, SocialActivity
 
-from backend.consts import INSTANCE_DIR
-from backend.db.models import Journal, db, User
+app = Flask(__name__)
 
-app = Flask(__name__, instance_path=os.path.abspath(INSTANCE_DIR))
-app.config["DATABASE"] = os.path.join(INSTANCE_DIR, "db.sqlite")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-try:
-    os.makedirs(app.instance_path)
-except FileExistsError:
-    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///welness-garden.db'
+db.init_app(app)
+
 
 @app.route('/is-alive')
 def is_alive():
     return 'Backend is alive!'
 
 
-@app.route('/journals/<int:journal_id>/')
-def get_journal(journal_id):
-    return Journal.query.get(journal_id).as_dict(), 200
+@app.route('/users/<username>')
+def users(username):
+    return User.query.filter_by(username=username).one_or_404().as_dict(), 200
+
+
+@app.route('/journals/<int:journal_id>/', methods=['GET', 'POST'])
+def journals(journal_id: int):
+    if request.method == 'POST':
+        body = request.get_json(silent=True)
+        title = body['title']
+        description = body['description']
+        journal: Journal = Journal(
+            title=title,
+            description=description)
+        db.session.add(journal)
+        db.session.commit()
+        return "success", 200
+    elif request.method == 'GET':
+        return Journal.query.filter_by(id=journal_id).first().as_dict(), 200
+
+
+@app.route('/activities/<int:user_id>/', methods=['GET', 'POST'])
+def activities(user_id: int):
+    if request.method == 'GET':
+        return Activity.query.filter_by(user_id=1), 200
+    elif request.method == 'POST':
+        body = request.get_json(silent=True)
+        name = body['title']
+        description = body['description']
+        points = body['description']
+        activity: Activity = Activity(name=name, description=description, points=points)
+        db.session.add(activity)
+        db.session.commit()
+        return "success", 200
+
+
+@app.route('/activities/<int:user_id>/<int:activity_id>/>', methods=['GET', 'PATCH'])
+def activity(user_id: int, activity_id: int):
+    if request.method == 'GET':
+        return Activity.query.filter_by(user_id=1, id=activity_id).first().as_dict(), 200
+    elif request.method == 'PATCH':
+        body = request.get_json(silent=True)
+        points = body['points']
+        activity: Activity = Activity.query.filter_by(user_id=1, id=activity_id).first()
+        activity.points += points
+        db.session.add(activity)
+        db.session.commit()
+        return "success", 200
+
+
+@app.route('/friends/<int:user_id>/', methods=['GET', 'POST'])
+def friends(user_id: int):
+    if request.method == 'GET':
+        return Friend.query.filter_by(user_id=1).first().friends, 200
+    elif request.method == 'POST':
+        body = request.get_json(silent=True)
+        name = body['name']
+        description = body['description']
+        friend: Friend = Friend(name=name, description=description)
+        db.session.add(friend)
+        db.session.commit()
+        return "success", 200
+
+
+@app.route('/socials/<int:user_id>/', methods=['GET', 'POST'])
+def socials(user_id: int):
+    if request.method == 'GET':
+        return SocialActivity.query.filter_by(user_id=1).first(), 200
+    elif request.method == 'POST':
+        body = request.get_json(silent=True)
+        name = body['name']
+        description = body['description']
+        mood = body['mood']
+        friend_id = body['friend_id']
+        social: SocialActivity = SocialActivity(name=name, description=description, mood=mood, friend_id=friend_id)
+        db.session.add(social)
+        db.session.commit()
+        return "success", 200
 
 
 if __name__ == '__main__':
-    db.init_app(app)
     with app.app_context():
         db.create_all()
-        if not User.query.get(1):
-            user = User(username="test", password="test", name="Test User")
+        if not User.query.filter_by(id=1).one_or_none():
+            user = User(username="buddy", password="qwerty", name="Study Buddy")
             db.session.add(user)
             db.session.commit()
     app.run()
--- backend/db/models.py
+++ backend/db/models.py
@@ -1,12 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 
-from backend.consts import MAX_MOOD_SCALE
-
 db = SQLAlchemy()
 
 friends_activity = db.Table('friends_activity',
     db.Column('friend_id', db.Integer, db.ForeignKey('friend.id'), primary_key=True),
-    db.Column('activity_id', db.Integer, db.ForeignKey('activity.id'), primary_key=True)
+    db.Column('social_activity_id', db.Integer, db.ForeignKey('social_activity.id'), primary_key=True)
 )
 
 class User(db.Model):
@@ -15,14 +13,18 @@ class User(db.Model):
     password = db.Column(db.String(80), nullable=False)
     name = db.Column(db.String(80), nullable=False)
 
+    def as_dict(self):
+        return {
+            "id": self.id,
+        }
+
 # Journal functionality
 class Journal(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(80), nullable=False)
     description = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('journals', lazy=True))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, default=1)
 
     def as_dict(self):
         return {
@@ -43,16 +45,6 @@ class Entry(db.Model):
     journal_id = db.Column(db.Integer, db.ForeignKey('journal.id'), nullable=False)
     journal = db.relationship('Journal', backref=db.backref('entries', lazy=True))
 
-    def as_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "content": self.content,
-            "creation_date": self.created_at,
-            "journal": self.journal.title,
-            "journal_id": self.journal.id
-        }
-
 
 # Activities functionality
 class Activity(db.Model):
@@ -61,19 +53,8 @@ class Activity(db.Model):
     description = db.Column(db.Text, nullable=False)
     points = db.Column(db.Integer, nullable=False, default=1)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, default=1)
     user = db.relationship('User', backref=db.backref('hobbies', lazy=True))
-
-    def as_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "points": self.points,
-            "creation_date": self.created_at,
-            "owner": self.user.username,
-            "moods": [mood.mood for mood in self.moods]
-        }
 
 
 class ActivityMood(db.Model):
@@ -83,15 +64,6 @@ class ActivityMood(db.Model):
     activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'), nullable=False)
     activity = db.relationship('Activity', backref=db.backref('moods', lazy=True))
 
-    def as_dict(self):
-        return {
-            "id": self.id,
-            "mood": self.mood,
-            "date": self.date,
-            "activity": self.activity.name,
-            "activity_id": self.activity.id
-        }
-
 
 # Friends tree functionality
 class Friend(db.Model):
@@ -99,19 +71,11 @@ class Friend(db.Model):
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, default=1)
     user = db.relationship('User', backref=db.backref('friends', lazy=True))
 
     def __repr__(self):
         return f'<Friend {self.name}>'
-
-    def as_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "creation_date": self.created_at
-        }
 
 
 class SocialActivity(db.Model):
@@ -120,16 +84,6 @@ class SocialActivity(db.Model):
     description = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
     mood = db.Column(db.Integer, nullable=False)
-    friends = db.relationship('Friend', secondary=friends_activity, lazy='dynamic', backref=db.backref('social_activities', lazy=True))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('social_activities', lazy=True))
-
-    def as_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "description": self.description,
-            "date": self.date,
-            "mood": self.mood,
-            "friends": [friend.name for friend in self.friends] # noqa
-        }
+    friends = db.relationship('Friend', secondary=friends_activity, lazy='dynamic', backref=db.backref('social_activity', lazy=True))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, default=1)
+    user = db.relationship('User', backref=db.backref('social_activity', lazy=True))
