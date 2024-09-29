ac5512a2993a25ca9ce7135a95b7e4d8e03395ce
Michał Krause <michalkr53@gmail.com>
2024-09-29 04:28:21 +0200

    Animations (#26)
    * Typography, theme, garden routing
    
    * Reworked routing, animations

--- frontend/wellness-garden/src/main.tsx
+++ frontend/wellness-garden/src/main.tsx
@@ -1,59 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider
-} from 'react-router-dom'
 import './static/css/index.css'
-import MainView from './components/views/MainView/MainView.tsx'
-import CssBaseline from '@mui/material/CssBaseline'
-import GardenView from './components/views/GardenView/GardenView.tsx'
 import { UserProvider } from './components/common/UserContext'
-import { createTheme, ThemeProvider } from '@mui/material'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#312F2F',
-    },
-    secondary: {
-      main: '#F1F5F9',
-    },
-  },
-  typography: {
-    fontFamily: "Fredoka, sans-serif",
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: '20px',
-          letterSpacing: "1px",
-          borderWidth: "2px",
-        }
-      }
-    }
-  }
-});
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainView />
-  },
-  {
-    path: "/garden",
-    element: <GardenView></GardenView>
-  }
-])
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <UserProvider>  
-        <RouterProvider router={router} />
+    <BrowserRouter>
+      <UserProvider>
+        <App />
       </UserProvider>
-    </ThemeProvider>
-  </StrictMode>,
+    </BrowserRouter>
+  </StrictMode>
 )
