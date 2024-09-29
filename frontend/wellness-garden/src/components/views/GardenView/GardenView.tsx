d101ec4ac8172b0a0fcdfdc652d34df3cb3b0d74
michalkr52 <michalkr53@gmail.com>
2024-09-29 04:18:58 +0200

    Reworked routing, animations

--- frontend/wellness-garden/src/components/views/GardenView/GardenView.tsx
+++ frontend/wellness-garden/src/components/views/GardenView/GardenView.tsx
@@ -3,6 +3,7 @@ import { Activity } from '../../../types';
 import Field from '../../garden/Field';
 import './GardenView.css';
 import SideNavButtons from '../../common/SideNavButtons';
+import { motion } from 'framer-motion';
 
 const GardenView = () => {
 	// todo implement logic to pick larger from fetch.length + x (x === 2 || 3 depending on fetch.length being even or odd ) or 8
@@ -43,16 +44,36 @@ const GardenView = () => {
 		<div className="view garden-view">
 			<div>
 				<div className="garden-view-bg">
-					<div className="bg-ground-grass"></div>
+					<motion.div className="bg-ground-grass"
+						initial={{ y: "80vh" }}
+						animate={{ y: 0 }}
+						exit={{ y: "80vh" }}
+						transition={{ duration: 1, ease: "easeInOut" }}
+					></motion.div>
 				</div>
-				<img src="/fence.png" alt="Garden fence" className="garden-view-fence" />
-				<div className='garden-view-field'>
+				<motion.img src="/fence.png" alt="Garden fence" className="garden-view-fence"
+					initial={{ y: "80vh" }}
+					animate={{ y: 0 }}
+					exit={{ y: "80vh" }}
+					transition={{ duration: 1, ease: "easeInOut" }}
+				/>
+				<motion.div className='garden-view-field'
+					initial={{ y: "80vh" }}
+					animate={{ y: 0 }}
+					exit={{ y: "80vh" }}
+					transition={{ duration: 1, ease: "easeInOut" }}
+				>
 					<Field activities={activities}></Field>
-				</div>
+				</motion.div>
 			</div>
-			<div className='garden-view-nav-buttons'>
+			<motion.div className='garden-view-nav-buttons'
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}
+				transition={{ duration: 0.5, ease: "easeInOut" }}
+			>
 				<SideNavButtons buttons={navButtons}></SideNavButtons>
-			</div>
+			</motion.div>
 		</div>
   )
 }
--- frontend/wellness-garden/src/main.tsx
+++ frontend/wellness-garden/src/main.tsx
@@ -1,56 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider
-} from 'react-router-dom'
 import './static/css/index.css'
-import MainView from './components/views/MainView/MainView.tsx'
-import CssBaseline from '@mui/material/CssBaseline'
-import GardenView from './components/views/GardenView/GardenView.tsx'
-import { createTheme, ThemeProvider } from '@mui/material'
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#312F2F',
-    },
-    secondary: {
-      main: '#F1F5F9',
-    },
-  },
-  typography: {
-    fontFamily: "Fredoka, sans-serif",
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: '20px',
-          letterSpacing: "1px",
-          borderWidth: "2px",
-        }
-      }
-    }
-  }
-});
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainView />
-  },
-  {
-    path: "/garden",
-    element: <GardenView></GardenView>
-  }
-])
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
