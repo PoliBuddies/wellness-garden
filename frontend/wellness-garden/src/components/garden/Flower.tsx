ad8dbdf55cefdea5b9363676d33a21d72d90cbdc
michalkr52 <michalkr53@gmail.com>
2024-09-29 11:10:37 +0200

    Mockup flower tiers

--- frontend/wellness-garden/src/components/garden/Flower.tsx
+++ frontend/wellness-garden/src/components/garden/Flower.tsx
@@ -3,9 +3,10 @@ import './garden.css';
 
 interface FlowerProps {
     id: number;
+    tier: number;
 }
 
-const Flower: FC<FlowerProps> = ({id}) => {
+const Flower: FC<FlowerProps> = ({id, tier}) => {
     const seededRandom = (seed: number): () => number => {
         let m = 0x80000000;
         let a = 1103515245;
@@ -23,6 +24,15 @@ const Flower: FC<FlowerProps> = ({id}) => {
     const getRandomFlowerImg = (): string => {
         const random = seededRandom(id);
         const randomNumber = Math.floor(random() * 8);
+        if (tier === 0) {           
+            return 'url("/flower_2_1.png")';
+        }
+        else if (tier === 2) {
+            return 'url("/flower_2_2.png")';
+        }
+        else if (tier === 3) {
+            return 'url("/flower_2_3.png")';
+        }
         switch (randomNumber) {
             case 0:
                 return 'url("/flower_1_1.png")';
